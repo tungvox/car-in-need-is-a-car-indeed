f91e9067aebf6855ea9efe9fcbfc1f36fdde6fc3
Tung Vo <tungvosgt@gmail.com>
2024-06-17 21:03:56 +0300

    Modify style

--- src/app/my-listings/page.tsx
+++ src/app/my-listings/page.tsx
@@ -56,7 +56,7 @@ const MyListings = () => {
       {listings.length > 0 ? (
         <VehicleList vehicles={listings} />
       ) : (
-        <Typography variant="body1">No listings found.</Typography>
+        <Typography variant="body1">You have no listings.</Typography>
       )}
     </Box>
   );
